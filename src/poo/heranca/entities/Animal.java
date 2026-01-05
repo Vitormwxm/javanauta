bc54d21a5fa39221c4925987e0ed8f63167346cf
vitor <vitormwxm@gmail.com>
2026-01-04 23:17:10 -0300

    Herança

---
+++ src/poo/heranca/entities/Animal.java
@@ -0,0 +1,12 @@
+package poo.heranca.entities;
+
+public class Animal {
+
+    public void comer(String alimento) {
+        System.out.println("O animal está comendo " + alimento);
+    }
+
+    public void dormir() {
+        System.out.println("O animal está dormingo ");
+    }
+}
---
+++ src/poo/heranca/entities/Cachorro.java
@@ -0,0 +1,8 @@
+package poo.heranca.entities;
+
+public class Cachorro extends Animal{
+
+    public void latir() {
+        System.out.println("O cachorro está latindo");
+    }
+}
